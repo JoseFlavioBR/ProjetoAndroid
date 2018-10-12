e4cfc3e640031ff9c8a43b3ca5279086dd8ecb1c
jose <joseplk000@hotmail.com>
2018-10-12 03:58:20 -0300

    Alterações no firebase
    Alterações no firebase e nos metodos mas tem um problema na utilziação do login para corrigir, faltando implementar so a navigation drawer

--- app/src/main/java/com/example/jose/vempravan/UserLogin.kt
+++ app/src/main/java/com/example/jose/vempravan/UserLogin.kt
@@ -1,13 +1,51 @@
 package com.example.jose.vempravan
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.EditText
+import android.widget.Toast
 import com.example.jose.vempravan.R
+import com.google.firebase.auth.FirebaseAuth
 
 class UserLogin : AppCompatActivity() {
+
+    lateinit var cpEmailUser : EditText
+    lateinit var cpPassworUser: EditText
+    lateinit var autorizacao : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_login)
+
+        cpEmailUser = findViewById(R.id.cpEmailUser)
+        cpPassworUser = findViewById(R.id.cpPasswordUser)
+        autorizacao = FirebaseAuth.getInstance()
+
     }
+
+    fun loginUsuario(){
+        val user: String = cpEmailUser.text.toString()
+        val password: String = cpPassworUser.text.toString()
+
+        if(!TextUtils.isEmpty(user) && TextUtils.isEmpty(password)){
+            autorizacao.signInWithEmailAndPassword(user, password).addOnCompleteListener(this){
+                task ->
+                if(task.isSuccessful){
+                    usuarioHome()
+                }else{
+                    Toast.makeText(this, "Erro na autenticação", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+
+    }
+
+    private fun usuarioHome(){
+        startActivity(Intent(this, UserHome::class.java))
+    }
+
+
+
 }
