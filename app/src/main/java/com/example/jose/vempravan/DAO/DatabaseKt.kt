840a803fe27b31c0d921a138cf4ff1f682560f09
jose <joseplk000@hotmail.com>
2018-10-15 15:05:47 -0300

    Login e Cadastro de Usuario FB
    Login e cadastro de usuario com o FIREBASE

--- app/src/main/java/com/example/jose/vempravan/DAO/DatabaseKt.kt
+++
@@ -1,7 +0,0 @@
-package com.example.jose.vempravan.DAO
-
-import com.google.firebase.database.FirebaseDatabase
-
-class DatabaseKt {
-
-}
--- app/src/main/java/com/example/jose/vempravan/DAO/UserDao.kt
+++
@@ -1,7 +0,0 @@
-package com.example.jose.vempravan.DAO
-
-import android.database.DatabaseUtils
-
-class UserDao (val id: String, val nome : String, val password: String,val password2: String, val placaVan:String?) {
-
-}
